9f1e3677843406b85cb6583f75fd52d974f5936b
chessrocks1357 <wangxikang0817@gmail.com>
2024-08-19 11:39:43 +0800

    app.js initial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Jason from './components/jason/json.js';
+import Arnav from './components/arnav/arnav.js';
+import Boaz from './components/boaz/boaz.js';
 
 function App() {
   return (
@@ -7,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          hello world! <br></br>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -17,6 +21,9 @@ function App() {
         >
           Learn React
         </a>
+        <Jason />
+        <Arnav />
+        <Boaz />
       </header>
     </div>
   );
---
+++ src/components/jason/json.js
@@ -0,0 +1,9 @@
+function Jason() {
+    return(
+        <div>
+            <p>what is my name?</p>
+        </div>
+    )
+}
+
+export default Jason
