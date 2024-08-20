f975b7042f3dd21e28a6e488d1fee84364702637
arcxne <arnav.sg@gmail.com>
2024-08-20 09:27:10 +0800

    Timer somewhat working

--- src/components/arnav/arnav.jsx
+++ src/components/arnav/arnav.jsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { useState, useEffect } from 'react';
 
 import {
@@ -16,23 +17,30 @@ import {
   useColorModeValue,
   Box,
   Progress,
+  Popover,
 } from '@chakra-ui/react'
 
 import ReusableButton from './reusableButton.jsx';
 
 function Arnav() {
-  const [initialTime, setInitialTime] = useState(0);
-  const [time, setTime] = useState(0);
+  const [initialTime, setInitialTime] = useState(10);
+  const [time, setTime] = useState(10);
   const [isRunning, setIsRunning] = useState(false);
   const [isCompleted, setIsCompleted] = useState(false);
   const [showStart, setShowStart] = useState(true);
   const [showResume, setShowResume] = useState(false);
+  const [showTimerButtons, setShowTimerButtons] = useState(true);
+
+  const changeTimer = () => {
+    setInitialTime(initialTime+10);
+  }
 
   const startTimer = () => {
-    setInitialTime(10);
-    setTime(initialTime);
-    setIsRunning(true);
-    setShowStart(false);
+    if (initialTime != 0) {
+      setIsRunning(true);
+      setShowStart(false);
+      setShowTimerButtons(false);
+    }
   }
   const pauseTimer = () => {
     setIsRunning(false);
@@ -47,15 +55,46 @@ function Arnav() {
     setShowStart(true);
     setShowResume(false);
     setTime(initialTime);
+    setShowTimerButtons(true);
   }
 
-  useEffect = () => ({
-    // if (isRunning) {
-    //   time -= 0.1;
-    //   console.log('hi')
-    //   // timer function
+  React.useEffect(() => {
+    // when updating time, check again if isRunning is true
+    isRunning && time > 0 && setTimeout(() => setTime(time - 1), 1000);
+    !isRunning && !showResume && setTime(initialTime);
+    console.log("running:", time);
+    // if (isRunning && time > 0) {
+    //   setTimeout(() => setTime(time - 1), 1000);
     // }
-  } [isRunning])
+  }, [time, isRunning]);
+
+  useEffect(() => {
+    setTime(initialTime);
+  }, [initialTime]);
+
+  // const timer = setInterval(function() {
+  //   if (isRunning && !isCompleted) {
+  //     setTime(time - 1);
+  //     console.log(time);
+  //   }
+  //   if (time === 0) {
+  //     clearInterval(timer);
+  //     setIsCompleted(true);
+  //   }
+  // }, 1000);
+
+  useEffect(() => {
+    setIsRunning(false);
+    if (isCompleted) console.log("Done!");
+  }, [isCompleted]);
+
+  // useEffect = () => (function() {
+  //   // if (isRunning) {
+  //   //   time -= 0.1;
+  //   //   console.log('hi')
+  //   // //   // timer function
+  //   // }
+  // } [isRunning]);
 
   console.log(isRunning);
 
@@ -64,40 +103,60 @@ function Arnav() {
       <Card
         overflow='hidden'
         variant='outline'
-        width='100%'
-        height='200px'
+        width='400px'
+        height='400px'
       >
         <CardBody>
-          
+          <Text contentEditable='true'>
+            {Math.floor(time / 60).toString().padStart(2, '0')}:{(time % 60).toString().padStart(2, '0')}
+          </Text>
+
+          {
+            showTimerButtons ? (
+              <>
+                <ReusableButton onClick={changeTimer}>
+                  +10s
+                </ReusableButton>
+                <ReusableButton onClick={changeTimer}>
+                  -10s
+                </ReusableButton>
+              </>
+            ) : (
+              <></>
+            )
+          }
         </CardBody>
 
         <CardFooter>
-          {
-            showStart ? (
-              <ReusableButton onClick={startTimer}>
-                Start
-              </ReusableButton>
-            ) : (
-              <>
-                {
-                  showResume ? (
-                    <ReusableButton onClick={resumeTimer}>
-                      Resume
-                    </ReusableButton>
-                  ) : (
-                    <ReusableButton onClick={pauseTimer}>
-                      Pause
-                    </ReusableButton>
-                  )
-                }
-                <ReusableButton onClick={restartTimer}>
-                  Restart
-                </ReusableButton>
-              </>
-            )
-          }
+          <Stack width='100%'>
 
-          <Progress value={initialTime-time/initialTime * 100} width='100%' />
+            {
+              showStart ? (
+                <ReusableButton onClick={startTimer}>
+                  Start
+                </ReusableButton>
+              ) : (
+                <>
+                  {
+                    showResume ? (
+                      <ReusableButton onClick={resumeTimer}>
+                        Resume
+                      </ReusableButton>
+                    ) : (
+                      <ReusableButton onClick={pauseTimer}>
+                        Pause
+                      </ReusableButton>
+                    )
+                  }
+                  <ReusableButton onClick={restartTimer}>
+                    Restart
+                  </ReusableButton>
+                </>
+              )
+            }
+
+            <Progress value={(initialTime-time)/initialTime * 100} width='100%' />
+          </Stack>
         </CardFooter>
       </Card>
     </ChakraProvider>
