0cc9429a70fa2f909b84696f380720878251c443
arcxne <arnav.sg@gmail.com>
2024-08-23 16:34:02 +0800

    Added default width

--- src/components/main/reusableButton.tsx
+++ src/components/main/reusableButton.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Button } from '@chakra-ui/react';
 
-const ReusableButton = ({ colorScheme, size, children, ...props }) => {
+const ReusableButton = ({ colorScheme, size, width, height, children, ...props }) => {
   return (
-    <Button colorScheme={colorScheme || 'teal'} size={size || 'md'} {...props}>
+    <Button colorScheme={colorScheme || 'teal'} size={size || 'md'} width={width || '100%'} {...props}>
       {children}
     </Button>
   );
