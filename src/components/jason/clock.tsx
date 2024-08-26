3769d5e9d585d3448d91892bd738a7016b286dbd
chessrocks1357 <wangxikang0817@gmail.com>
2024-08-26 15:19:47 +0800

    displays the time now

--- src/components/jason/clock.tsx
+++ src/components/jason/clock.tsx
@@ -7,10 +7,31 @@ import { ChakraProvider, Text, Card, CardBody } from "@chakra-ui/react";
 
 function Time() {
   const [value, setValue] = useState(new Date());
+  const [formattedtime, setFormattedTime] = useState("");
 
   useEffect(() => {
-    const interval = setInterval(() => setValue(new Date()), 1000);
-    console.log(value);
+    const interval = setInterval(() => {
+      let date = new Date();
+      setValue(date);
+      let hours = date.getHours();
+      let minutes = date.getMinutes();
+      let seconds = date.getSeconds();
+
+      // Check whether AM or PM
+      let newformat = hours >= 12 ? "PM" : "AM";
+
+      // Find current hour in AM-PM Format
+      hours = hours % 12;
+
+      // To display "0" as "12"
+      hours = hours ? hours : 12;
+      let minutes2 = minutes < 10 ? "0" + minutes : minutes;
+      let seconds2 = seconds < 10 ? "0" + seconds : seconds;
+
+      setFormattedTime(
+        hours + ":" + minutes2 + ":" + seconds2 + " " + newformat
+      );
+    }, 1000);
 
     return () => {
       clearInterval(interval);
@@ -24,13 +45,14 @@ function Time() {
           <Clock
             value={value}
             renderNumbers={true}
-            size={300} // Increase the size of the clock    
+            size={300} // Increase the size of the clock
             renderMinuteMarks={true}
             minuteMarksLength={5}
           />
+          <Text color="black" paddingTop="10px">
+            {formattedtime}
+          </Text>
         </CardBody>
-
-        {/* <Text color='black'>{value.toLocaleString()}</Text> */}
       </Card>
     </ChakraProvider>
   );
