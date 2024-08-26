e4f0881d3213f6c09da43a7a41875b446aa6abbf
arcxne <arnav.sg@gmail.com>
2024-08-26 11:23:00 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 import Jason from './components/jason/json.tsx';
-import Arnav from './components/arnav/arnav.js';
+import Arnav from './components/arnav/arnav.tsx';
 import Boaz from './components/boaz/boaz.js';
+import { useEffect } from 'react';
+import { createSwapy } from 'swapy';
 
 function App() {
+  useEffect(() => {
+    const container = document.querySelector('.container');
+    const swapy = createSwapy(container, {
+      animation: 'dynamic' // or spring or none
+    });
+    swapy.enable(true);
+
+    // Clean up on unmount
+    return () => {
+      swapy.enable(false);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world! <br></br>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Jason />
-        <Arnav />
-        <Boaz />
+        
+        <div className="container">
+          <div className="section-1" data-swapy-slot="slot1">
+            <div className="content-a" data-swapy-item="itemA">
+              <Jason />
+            </div>
+          </div>
+
+          <div className="section-2" data-swapy-slot="slot2">
+            <div className="content-b" data-swapy-item="itemB">
+              <Arnav />
+            </div>
+          </div>
+
+          <div className="section-3" data-swapy-slot="slot3">
+            <div className="content-c" data-swapy-item="itemC">
+              <Arnav />
+            </div>
+          </div>
+
+          <div className="section-4" data-swapy-slot="slot4">
+            <div className="content-d" data-swapy-item="itemD">
+              <Arnav />
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
