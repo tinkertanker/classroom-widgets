8e3f84bda8547e4714292a6bdab0afea47a141c4
arcxne <arnav.sg@gmail.com>
2024-08-28 16:31:44 +0800

    Fine Jason

--- src/App.js
+++ src/App.js
@@ -10,48 +10,52 @@ import Confetti from 'react-confetti';
 
 function App() {
   const [useconfetti, setUseconfetti] = useState(false);
+  const N = 2;
+  const M = 3;
 
   const toggleConfetti = (hi) => {
     setUseconfetti(hi);
   };
 
   useEffect(() => {
+    document.documentElement.style.setProperty('--grid-columns', N);
+    document.documentElement.style.setProperty('--grid-rows', M);
+
     const container = document.querySelector('.container');
     const swapy = createSwapy(container, {
-      animation: 'spring' // or spring or none
+      animation: 'spring' // or 'spring' or 'none'
     });
     swapy.enable(true);
 
-    // Clean up on unmount
     return () => {
       swapy.enable(false);
     };
-  }, []);
+  }, [N, M]);
 
   return (
     <div className="App">
       <header className="App-header">
         
         <div className="container">
-          <div className="section-1" data-swapy-slot="slot1">
+          <div className="slot section-1" data-swapy-slot="slot1">
             <div className="content-a" data-swapy-item="itemA">
               <Jason toggleConfetti={toggleConfetti}/>
             </div>
           </div>
 
-          <div className="section-2" data-swapy-slot="slot2">
+          <div className="slot section-2" data-swapy-slot="slot2">
             <div className="content-b" data-swapy-item="itemB">
               <Arnav />
             </div>
           </div>
 
-          <div className="section-3" data-swapy-slot="slot3">
+          <div className="slot section-3" data-swapy-slot="slot3">
             <div className="content-c" data-swapy-item="itemC">
               <List toggleConfetti={toggleConfetti} useConfetti={useconfetti} />
             </div>
           </div>
 
-          <div className="section-4" data-swapy-slot="slot4">
+          <div className="slot section-4" data-swapy-slot="slot4">
             <div className="content-d" data-swapy-item="itemD">
               <Arnav />
             </div>
