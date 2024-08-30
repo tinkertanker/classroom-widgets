cc894d732790ab4ac6bae1128bcb7a1ca72a59cb
boaznumberd5555 <26yboaz236j@student.ri.edu.sg>
2024-08-30 13:55:04 +0800

    boaz moment :sunglasses:

--- src/App.js
+++ src/App.js
@@ -10,49 +10,15 @@ import { Box, ChakraProvider, Heading } from '@chakra-ui/react'; import { useEff
 import { createSwapy } from 'swapy';
 import InitialPopup from './components/boaz/InitialPopup.tsx';
 function App() {
-  useEffect(() => {
-    const container = document.querySelector('.container');
-    const swapy = createSwapy(container, {
-      animation: 'dynamic' // or spring or none
-    });
-    swapy.enable(true);
-
-    // Clean up on unmount
-    return () => {
-      swapy.enable(false);
-    };
-  }, []);
-
   return (
     <Box className="App" width="100%">
       <Heading className="App-header" width="100%" >
         <ChakraProvider>
           <InitialPopup />
-          <div className="container">
-            <div className="section-1" data-swapy-slot="slot1">
-              <div className="content-a" data-swapy-item="itemA">
-                <Boaz />
-              </div>
-            </div>
-
-            <div className="section-2" data-swapy-slot="slot2">
-              <div className="content-b" data-swapy-item="itemB">
-                <AudioVolumeMonitor />
-              </div>
-            </div>
-
-            <div className="section-3" data-swapy-slot="slot3">
-              <div className="content-c" data-swapy-item="itemC">
-                <TrafficLight />
-              </div>
-            </div>
-
-            <div className="section-4" data-swapy-slot="slot4">
-              <div className="content-d" data-swapy-item="itemD">
-                <ShortenLink />
-              </div>
-            </div>
-          </div>
+          <Boaz />
+          <AudioVolumeMonitor />
+          <TrafficLight />
+          <ShortenLink />
         </ChakraProvider>
       </Heading>
     </Box >
